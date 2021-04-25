94291cde4e3619dc7ab806b1bf9ee44ba94d6ddb
Matthew Eddy <mattheweddy@Matthews-Air.attlocal.net>
2021-04-24 22:19:17 -0500

    refactored fibonnaci function

--- src/pages/Fibonacci/index.jsx
+++ src/pages/Fibonacci/index.jsx
@@ -21,51 +21,32 @@ function Fibonacci({ content }) {
     const xValue = e.target.x.value;
     const yValue = e.target.y.value;
     const zValue = e.target.z.value;
-    fibonacci(xValue, yValue, zValue);
+    fibonacciPlus(xValue, yValue, zValue);
   }
 
   /**
-   * 
+   * the function returns two different sequences:
+   * If the y and z values are defined F(x) = F(x - y) + F(x - z)
+   * If y and z are not defined it will perform the regular Fibonacci sequence
+   * which is F(n) = F(n -1) + F(n - 2).
    */
-  function fibonacci(x, y, z) {
+  function fibonacciPlus(x, y, z) {
     let fibos = [];
     fibos[0] = 1;
     fibos[1] = 1;
     fibos[2] = 2;
     if (y !== '' && z !== '') {
       for (let i = 3; i < x; i++){
-        fibos[i] = fibos[x -y] + fibos[x - z]
+        const res = fibos[i - y] + fibos[i - z];
+        fibos[i] = res ? res : 1;
       }
     } else {
       for (let i = 3; i < x; i++){
         fibos[i] = fibos[i -1] + fibos[i -2]
       }
     }
-    setResults(results);
+    setResults(fibos);
   }
-  
-  // function Test(x, y, z) {
-  //   let sequence = [0]; // f(0) = 0
-  //   let v1,             // f(x-y)
-  //       v2;             // f(x-z)
-    
-  //   for (let i=1; i < x; i++) {
-  //     // f(x - y)
-  //     v1 = sequence[i - y];
-  //     // f(x-z)
-  //     v2 = sequence[i - z];
-      
-  //     // If a number does not exist in the sequence output a 1 instead
-  //     if (typeof v1 === 'undefined')
-  //       v1 = 1;
-  //     if (typeof v2 === 'undefined')
-  //       v2 = 1;
-  
-  //     sequence.push(v1 + v2)
-  //   }
-    
-  //   return sequence
-  // }
 
   return (
     <div className="Fibonacci">
